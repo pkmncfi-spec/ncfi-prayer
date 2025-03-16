fdde150a2ef979d5da963e95e4b93cc3101d3aac
pejuangvscode <151711997+pejuangvscode@users.noreply.github.com>
2025-03-16 21:42:54 +0700

    test deploy

--- src/pages/test.tsx
+++
@@ -1,7 +0,0 @@
-export default function RegisterPage() {
-    return (
-        <div className="bg-gradient-to-r from-blue-500 to-purple-500 h-screen">
-        {/* Konten Anda di sini */}
-        </div>
-    );
-}

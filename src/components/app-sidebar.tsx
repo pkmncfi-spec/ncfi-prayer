166b183bba2addea0265b426aca1d46b336d7ad7
pejuangvscode <151711997+pejuangvscode@users.noreply.github.com>
2025-03-20 15:09:10 +0700

    search page addes

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -15,29 +15,14 @@ import {
 const items = [
   {
     title: "Home",
-    url: "#",
+    url: "/home",
     icon: Home,
   },
   {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
-  },
-  {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
-  },
-  {
     title: "Search",
-    url: "#",
+    url: "/search",
     icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
+  }
 ]
 
 export function AppSidebar() {
---
+++ src/pages/search.tsx
@@ -0,0 +1,3 @@
+export default function SearchPage() {
+    return <h1 className="text-5xl">INI NANTI ADALAH SEARCH PAGES</h1>;
+}

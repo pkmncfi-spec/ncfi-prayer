161e92222031c5b97338a3e06a88c4cadf127c9f
Bedul <151714133+gggrc@users.noreply.github.com>
2025-03-22 17:29:49 +0700

    Update profile.tsx

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -47,14 +47,7 @@ export default function ProfilePage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-    
       <main className="flex flex-col w-full max-w-[500px] mx-autoborder min-h-screen">
-        <div className="flex flex-col w-full items-center justify-center">
-          <button>
-            <div className="absolute left-1/2 top-1/2 w-6 h-6 bg-gray-400 rounded-full transform -translate-x-1/2 -translate-y-1/2"></div>
-          </button>
-        </div>
-
         <div className="bg-gray-400 h-24 flex justify-center"></div>
         <div className="relative flex flex-col items-right -mt-12 p-4">
           <div className="w-20 h-20 bg-gray-300 rounded-full"></div>
@@ -88,7 +81,6 @@ export default function ProfilePage() {
           <h3 className="text-sm font-bold mb-2">Your Posts</h3>
           {posts.map((post) => (
             <div key={post.id} className="bg-white p-3 rounded-lg flex items-start border border-gray-300 shadow-sm mb-2">
-              <div className="w-8 h-8 bg-gray-300 rounded-full mr-3"></div>
               <div className="flex-1">
                 <p className="font-bold text-sm">{post.author} <span className="text-gray-500 font-normal">· {post.date}</span></p>
                 <p className="text-gray-700 text-xs">{post.content} <a href="#" className="text-blue-600 font-medium">...click to see more</a></p>

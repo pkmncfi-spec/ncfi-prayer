5c023d1f6a62b7375774ddfd2acf61d2a0512082
Bedul <151714133+gggrc@users.noreply.github.com>
2025-03-22 16:29:00 +0700

    Update search.tsx

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -8,10 +8,10 @@ export default function Page() {
   return (
     <Layout>
       <SidebarTrigger />
-      <main className="flex flex-col w-full max-w-[500px] mx-auto min-h-screen flex-grow">
-        <Card className="flex flex-col w-full items-center justify-start flex-grow">
+      <main className="flex flex-col w-full max-w-[500px] mx-auto min-h-screen overflow-auto">
+        <Card className="flex flex-col w-full items-center justify-start flex-1">
           {/* Header */}
-          <div className="relative w-full h-16 bg-gray-000 flex items-center px-4 border-b border-gray-300">
+          <div className="relative w-full h-16 bg-gray-100 flex items-center px-4 border-b border-gray-300">
             <div className="absolute left-1/2 top-1/2 w-6 h-6 bg-gray-400 rounded-full transform -translate-x-1/2 -translate-y-1/2"></div>
           </div>
 

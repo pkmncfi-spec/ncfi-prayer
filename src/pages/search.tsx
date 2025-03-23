380df79d1cc2031d880c3af293e60b2a9296fd3f
Bedul <151714133+gggrc@users.noreply.github.com>
2025-03-23 21:21:42 +0700

    Update search.tsx

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -9,7 +9,7 @@ export default function Page() {
       <SidebarTrigger />
       <main className="flex flex-col w-full max-w-[500px] mx-autoborder min-h-screen">
         {/* Header */}
-        <div className="relative w-full h-16 bg-gray-100 flex items-center px-4 border-b border-gray-300">
+        <div className="relative w-full h-16 bg-gray-000 flex items-center px-4 border-b border-gray-300">
           <div className="absolute left-1/2 top-1/2 w-6 h-6 bg-gray-400 rounded-full transform -translate-x-1/2 -translate-y-1/2"></div>
         </div>
 
